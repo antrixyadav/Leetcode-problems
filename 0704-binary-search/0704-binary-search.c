c04238e67dd18156b0b6aaf7756ce25d44af66ea
Antrix yadav <antrixyadav10@gmail.com>
2026-09-24 23:02:40 +0530

    Time: 0 ms (100%), Space: 10.1 MB (44.12%) - LeetHub-Neo

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,14 @@
+int search(int* nums, int numsSize, int target) {
+    int lb=0,ub=numsSize-1,mid;
+    while(lb<=ub)
+    {
+        mid=(lb+ub)/2;
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target)
+            lb = mid + 1;
+        else
+            ub = mid - 1;
+    }
+    return -1;
+}
